7b7c32f8e67e58daeb0c64452ef038ca93e36235
unknown <wj24021040@163.com>
2022-03-21 16:43:14 +0800

    add interruptable-copy func

---
+++ io/interruptable-copy.go
@@ -0,0 +1,26 @@
+package io
+
+//golang标准库里的Copy，是基于dst来判断复制是否继续，InerruptableCopy可以通过context，从外部控制流复制
+
+import (
+	"context"
+	"io"
+)
+
+type readerFunc func(p []byte) (n int, err error)
+
+func (rdf readerFunc) Read(p []byte) (n int, err error) {
+	return rdf(p)
+}
+
+func InerruptableCopy(ctx context.Context, dst io.Writer, src io.Reader) (int64, error) {
+	n, err := io.Copy(dst, readerFunc(func(p []byte) (int, error) {
+		select {
+		case <-ctx.Done():
+			return 0, ctx.Err()
+		default:
+			return src.Read(p)
+		}
+	}))
+	return n, err
+}
